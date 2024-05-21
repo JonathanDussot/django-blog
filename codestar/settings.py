d3154a6ac4ace0a3cc3d4011cbfa39735c0de812
JonathanDussot <jonathandussot@gmail.com>
2024-05-21 13:40:12 +0000

    Add comment model to models.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jonathanduss-djangoblog-5ie7qiv3gyn.ws-eu111.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-jonathanduss-djangoblog-5ie7qiv3gyn.ws-eu114.gitpod.io','.herokuapp.com']
 
 
 # Application definition
